b71c618a38a14d1d0027fecb0a19cfdba0d6e8e1
Tim Finley <tfinley@hubspot.com>
2015-05-04 15:28:40 -0400

    First pass

---
+++ index.js
@@ -0,0 +1,33 @@
+var fs   = require('fs');
+var path = require('path');
+var mkdirp = require('mkdirp').sync;
+var symlinkOrCopySync = require('symlink-or-copy').sync;
+
+function BroccoliExtractSingleDirectory (inputTree, pathToExtract) {
+  if (!(this instanceof BroccoliExtractSingleDirectory)) {
+    return new BroccoliExtractSingleDirectory(inputTree, pathToExtract);
+  }
+
+  this.inputTree = inputTree;
+  this.pathToExtract = pathToExtract;
+}
+
+BroccoliExtractSingleDirectory.prototype.rebuild = function () {
+  var fullPathToExtract = path.join(this.inputPath, this.pathToExtract);
+  var fullDestPath = path.join(this.outputPath, this.pathToExtract);
+
+  var stats = fs.statSync(fullPathToExtract);
+
+  if (!stats.isDirectory()) {
+    throw new Error("Path to extract isn't a directory:", fullPathToExtract);
+  }
+
+  mkdirp(path.dirname(fullDestPath));
+  symlinkOrCopySync(fullPathToExtract, fullDestPath)
+  console.log('Pulled out', this.pathToExtract, 'to', fullDestPath);
+
+  return this.outputPath;
+}
+
+
+module.exports = BroccoliExtractSingleDirectory;
