c951f03d17c0ee7f700e5c475c8fd421f46f46e6
Tim Finley <tfinley@hubspot.com>
2015-05-08 11:39:36 -0400

    options.allowMissing (that defaults to true)

--- index.js
+++ index.js
@@ -3,20 +3,41 @@ var path = require('path');
 var mkdirp = require('mkdirp').sync;
 var symlinkOrCopySync = require('symlink-or-copy').sync;
 
-function BroccoliExtractSingleDirectory (inputTree, pathToExtract) {
+function BroccoliExtractSingleDirectory (inputTree, pathToExtract, options) {
   if (!(this instanceof BroccoliExtractSingleDirectory)) {
     return new BroccoliExtractSingleDirectory(inputTree, pathToExtract);
   }
 
+
+
+
   this.inputTree = inputTree;
   this.pathToExtract = pathToExtract;
+  this.options = options || {};
+
+  this.allowMissing = this.options.allowMissing;
+
+  if (this.allowMissing === undefined) {
+    this.allowMissing = true;
+  }
 }
 
 BroccoliExtractSingleDirectory.prototype.rebuild = function () {
   var fullPathToExtract = path.join(this.inputPath, this.pathToExtract);
   var fullDestPath = path.join(this.outputPath, this.pathToExtract);
+  var stats;
 
-  var stats = fs.statSync(fullPathToExtract);
+  try {
+    stats = fs.statSync(fullPathToExtract);
+  } catch (error) {
+    // If options.allowMissing is true, don't die if the directory is missing
+    if (error.code === 'ENOENT' && this.allowMissing === true) {
+      return this.outputPath;
+    } else {
+      console.error("Cannot extract", this.pathToExtract, "it is missing and options.allowMissing is _not_ true");
+      throw error;
+    }
+  }
 
   if (!stats.isDirectory()) {
     throw new Error("Path to extract isn't a directory:", fullPathToExtract);
